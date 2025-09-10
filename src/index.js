0a08326ac2b1db5795e52c7fdedb14141f069b82
Guilherme Augusto <guilhermedev23@gmail.com>
2025-09-10 12:08:43 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ const player1 = {
     MANOBRABILIDADE: 3,
     PODER: 3,
     PONTOS:0,
-
 }
+ const player2 = {
+    NOME: "luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS:0,
+ }
+ async function rollDice(){
+ return Math.floor(Math.random() * 6  ) + 1;
+} 
+
+
+(async function  main(){
+    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME}  Começando... \n`)
+})();
