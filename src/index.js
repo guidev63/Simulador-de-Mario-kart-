7597411bce0b6861492e1ce843f391aae7524a9d
Guilherme Augusto <guilhermedev23@gmail.com>
2025-09-18 17:50:05 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -3,20 +3,29 @@ const player1 = {
     VELOCIDADE: 4,
     MANOBRABILIDADE: 3,
     PODER: 3,
-    PONTOS:0,
+    PONTOS: 0,
 }
- const player2 = {
+const player2 = {
     NOME: "luigi",
     VELOCIDADE: 3,
     MANOBRABILIDADE: 4,
     PODER: 4,
-    PONTOS:0,
- }
- async function rollDice(){
- return Math.floor(Math.random() * 6  ) + 1;
-} 
+    PONTOS: 0,
+}
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
 
+async function playRaceEngine(character1, character2) {
+  for (let round = 1; round <= 5; round++) {
+    console.log(`🏁 Rodada ${round}`);
 
-(async function  main(){
-    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME}  Começando... \n`)
+    // sortear bloco
+  }
+}
+
+(async function main() {
+    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME}  Começando... \n`);
+
+    await playRaceEngine(player1, player2)
 })();
